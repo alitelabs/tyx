5da2ed7ed85256a0c54aa7e35c4fb030fa397041
Kostadin Bajalcaliev <kostadin.bajalcaliev@alite-international.com>
2018-05-12 15:44:57 +0200

    TypeORM

---
+++ src/orm/decorators.ts
@@ -0,0 +1,115 @@
+import { Column, ColumnOptions, PrimaryColumn, VersionColumn } from "typeorm";
+
+export function PrimaryIdColumn(options?: ColumnOptions) {
+    let op: ColumnOptions = { type: "varchar", length: 36 };
+    Object.assign(op, options || {});
+    return PrimaryColumn(op);
+}
+
+export function PrimaryLongColumn(options?: ColumnOptions) {
+    let op: ColumnOptions = { type: "bigint", length: 18 };
+    Object.assign(op, options || {});
+    return PrimaryColumn(op);
+}
+
+export function IdColumn(options?: ColumnOptions) {
+    let op: ColumnOptions = { type: "varchar", length: 36 };
+    Object.assign(op, options || {});
+    return Column(op);
+}
+
+export function StringColumn(options?: number | ColumnOptions) {
+    let op: ColumnOptions;
+    if (typeof options === "number") {
+        op = { type: "varchar", length: options || 255 };
+    } else if (typeof options === "object") {
+        op = { type: "varchar" };
+        Object.assign(op, options);
+    } else {
+        op = { type: "varchar" };
+    }
+    return Column(op);
+}
+
+export function TextColumn(options?: ColumnOptions) {
+    let op: ColumnOptions;
+    if (typeof options === "object") {
+        op = { type: "mediumtext" };
+        Object.assign(op, options);
+    } else {
+        op = { type: "mediumtext" };
+    }
+    return Column(op);
+}
+
+export function IntColumn(options?: number | ColumnOptions) {
+    let op: ColumnOptions;
+    if (typeof options === "number") {
+        op = { type: "int", length: options };
+    } else if (typeof options === "object") {
+        op = { type: "int", length: 11 };
+        Object.assign(op, options);
+    } else {
+        op = { type: "int", length: 11 };
+    }
+    return Column(op);
+}
+
+export function LongColumn(options?: number | ColumnOptions) {
+    let op: ColumnOptions;
+    if (typeof options === "number") {
+        op = { type: "bigint", length: options };
+    } else if (typeof options === "object") {
+        op = { type: "bigint", length: 18 };
+        Object.assign(op, options);
+    } else {
+        op = { type: "bigint", length: 18 };
+    }
+    return Column(op);
+}
+
+export function DoubleColumn(options?: ColumnOptions) {
+    let op: ColumnOptions;
+    if (typeof options === "object") {
+        op = { type: "double" };
+        Object.assign(op, options);
+    } else {
+        op = { type: "double" };
+    }
+    return Column(op);
+}
+
+export function BooleanColumn(options?: ColumnOptions) {
+    let op: ColumnOptions = { type: "tinyint", length: 1 };
+    if (typeof options === "object") {
+        Object.assign(op, options);
+    }
+    return Column(op);
+}
+
+export function TimestampColumn(options?: ColumnOptions) {
+    let op: ColumnOptions = { type: "datetime" };
+    if (typeof options === "object") {
+        Object.assign(op, options);
+    }
+    return Column(op);
+}
+
+export function LongVersionColumn(options?: ColumnOptions) {
+    let op: ColumnOptions;
+    if (typeof options === "number") {
+        op = { type: "bigint", length: options };
+    } else if (typeof options === "object") {
+        op = { type: "bigint", length: 18 };
+        Object.assign(op, options);
+    } else {
+        op = { type: "bigint", length: 18 };
+    }
+    return VersionColumn(op);
+}
+
+export function Transient() {
+    return function (type: any, propertyKey: string) {
+        // DO NOTHING
+    };
+}
