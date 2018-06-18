3bb82908678eba8d6d5d1591578bd8bc3cf295ed
Kostadin Bajalcaliev <kbajalc@gmail.com>
2018-06-18 22:31:43 +0200

    Support @Field without ()

--- src/decorators/type.ts
+++ src/decorators/type.ts
@@ -64,25 +64,31 @@ export function Enum(target: Object, name?: string): EnumMetadata {
 //     };
 // }
 
+export function Field(target: Object, propertyKey: string): void;
 export function Field<T = any>(): PropertyDecorator;
 export function Field<T = any>(type: VarType<T> | 0): PropertyDecorator;
 export function Field<T = any>(required?: boolean): PropertyDecorator;
 export function Field<T = any>(type?: VarType<T> | 0, required?: boolean): PropertyDecorator;
-export function Field<T = any>(typeOrRequired?: VarType<T> | 0 | boolean, isReq?: boolean): PropertyDecorator {
-  return (target, propertyKey) => {
+export function Field<T = any>(first?: VarType<T> | 0 | boolean | Object, second?: boolean | string | symbol): PropertyDecorator | void {
+  if (typeof second === 'symbol') throw new TypeError('propertyKey must be string');
+  if (typeof first === 'object' && typeof second === 'string') {
+    return void decorator(first, second);
+  }
+  return decorator;
+  function decorator(target: Object, propertyKey: string | symbol) {
     let type: VarType<T> = undefined;
     let required = false;
-    if (typeof typeOrRequired === 'boolean') {
-      required = typeOrRequired;
-    } else if (typeOrRequired === 0) {
+    if (typeof first === 'boolean') {
+      required = first;
+    } else if (first === 0) {
       type = Int;
-      required = isReq;
+      required = second as any;
     } else {
-      type = typeOrRequired;
-      required = isReq;
+      type = first as any;
+      required = second as any;
     }
     if (typeof propertyKey !== 'string') throw new TypeError('propertyKey must be string');
     Registry.trace(Field, { type, required }, target, propertyKey);
     TypeMetadata.define(target.constructor).addField(propertyKey, type, required);
-  };
+  }
 }
