633d30ff4d77237c66811ae219cc5c0cd4f84f3a
Kostadin Bajalcaliev <kbajalc@gmail.com>
2019-03-11 20:42:26 +0100

    Imediate close of express service

--- src/express/service.ts
+++ src/express/service.ts
@@ -1,45 +1,30 @@
 
 import { Server } from 'http';
-import { Release, Service } from '../decorators/service';
 import { Aws, Express } from '../import';
 import { Context } from '../types/core';
 import { HttpRequest, HttpResponse } from '../types/http';
 
-@Service(false)
 export abstract class ExpressService {
-
-  private server: Server;
 
   protected async process(ctx: Context, req: HttpRequest): Promise<HttpResponse> {
     let rsrc = req.resource;
     if (rsrc.indexOf('{') > 0) rsrc = rsrc.substring(0, rsrc.indexOf('{') - 1);
     req.path = req.path.substring(req.path.indexOf(rsrc));
-
     const app: Express.Express = Express.Create();
-    // app.use(bodyParser.json());
-    // app.use(awsServerlessExpressMiddleware.eventContext());
-
-    this.setup(app, ctx, req);
-
-    // TODO: Complete response
-    return new Promise<HttpResponse>((resolve, reject) => {
-      this.server = Aws.ServerlessExpress.createServer(app);
-      Aws.ServerlessExpress.proxy(this.server, req, {
-        succeed: (input: any) => resolve({
-          statusCode: input.statusCode,
-          headers: input.headers,
-          body: input.body,
-        }),
-        fail: (err: any) => reject(err),
-      } as any);
-    });
+    await this.prepare(app, ctx, req);
+    let server: Server;
+    let response: HttpResponse;
+    try {
+      server = Aws.ServerlessExpress.createServer(app);
+      response = await Aws.ServerlessExpress.proxy(server, req, {} as any, 'PROMISE').promise as any;
+    } finally {
+      if (server) server.close(() => void 0);
+      await this.destroy(app, ctx, response);
+    }
+    return response;
   }
 
-  protected abstract setup(app: Express.Express, ctx: Context, req: HttpRequest): void;
+  protected abstract async prepare(app: Express.Express, ctx: Context, req: HttpRequest): Promise<void>;
 
-  @Release()
-  public async release(): Promise<void> {
-    if (this.server) this.server.close();
-    this.server = undefined;
-  }
+  protected abstract async destroy(app?: Express.Express, ctx?: Context, res?: HttpResponse): Promise<void>;
 }
