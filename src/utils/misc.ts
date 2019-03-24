636be42dffc1134a60cce988e8dc74e6c074baf2
Kostadin Bajalcaliev <kbajalc@gmail.com>
2019-03-24 10:18:47 +0100

    Fix tslint

--- src/utils/misc.ts
+++ src/utils/misc.ts
@@ -171,7 +171,7 @@ export function interfaces() {
   Object.keys(ifaces).forEach((ifname) => {
     let alias = 0;
     ifaces[ifname].forEach((iface) => {
-      if ('IPv4' !== iface.family || iface.internal !== false) {
+      if ('IPv4' !== iface.family || iface.internal) {
         // skip over internal (i.e. 127.0.0.1) and non-ipv4 addresses
         return;
       }
